2470d897456e8dd9305b517733ec2d862116cb7c
ofisch <onni.fischer@gmail.com>
2023-05-02 16:18:24 +0300

    turhia juttuja siivottu

--- my-app/src/views/Upload.jsx
+++ my-app/src/views/Upload.jsx
@@ -42,27 +42,6 @@ const Upload = (props) => {
     console.log('inputs.', inputs.category);
   };
 
-  const handleToggle = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
-
-  const handleClose = (event) => {
-    if (anchorRef.current && anchorRef.current.contains(event.target)) {
-      return;
-    }
-
-    setOpen(false);
-  };
-
-  function handleListKeyDown(event) {
-    if (event.key === 'Tab') {
-      event.preventDefault();
-      setOpen(false);
-    } else if (event.key === 'Escape') {
-      setOpen(false);
-    }
-  }
-
   // return focus to the button when we transitioned from !open -> open
   const prevOpen = React.useRef(open);
   React.useEffect(() => {
